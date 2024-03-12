465c4612df6cba7873a8ae08ed20ec948ee8f58e
Marion Robert <mrobert148@gmail.com>
2024-03-12 15:29:36 -0400

    test isAlive and generate functions

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,10 +1,10 @@
 const { TestEnvironment } = require("jest-environment-jsdom");
 
 require("../index");
-const { isAlive } = window.game;
+const { isAlive, generate } = window.game;
 
 describe("game of life", () => {
-  describe("isAlive algorithm", () => {
+  describe("-- isAlive algorithm --", () => {
     test("dead cell with no living neighbours stays dead --> return 0", () => {
       expect(isAlive(0, 0)).toEqual(0);
     });
@@ -17,5 +17,15 @@ describe("game of life", () => {
     test("live cell with 2 living neighbours stays alive --> return 1", () => {
       expect(isAlive(1, 2)).toEqual(1);
     })
+    test("dead cell with 2 living neighbours stays alive --> return 0", () => {
+      expect(isAlive(0, 2)).toEqual(0);
+    })
   });
+
+  describe("-- generate function --", () => {
+    test("should create an array of x * x", () => {
+      expect(generate(1)).toEqual([0]);
+      expect(generate(2)).toEqual([0, 0, 0, 0]);
+    })
+  })
 });
--- index.js
+++ index.js
@@ -7,7 +7,9 @@
 
 const isAlive = (cell, neighbours) => neighbours === 3 || (Boolean(cell) && neighbours === 2) ? 1 : 0;
 
+const generate = (root) => new Array(root * root).fill(0);
 
 window.game = {
-  isAlive
+  isAlive,
+  generate
 };
